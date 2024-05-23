b2dc888aaba13591c2e2a816a3bbd21cccb662da
astro <sjromanovich@gmail.com>
2024-05-23 17:50:47 +0500

    index.php

--- info.php
+++ info.php
@@ -1,2 +1,13 @@
-<?php
-	echo php_info();
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>phpinfo</title>
+</head>
+<body>
+    <?php
+		phpinfo();
+	?>
+</body>
+</html>
